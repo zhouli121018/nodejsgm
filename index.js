24c8568f8486a32f5d56ae2b5398826391a7a277
zhouli121018 <17607619682@163.com>
2017-12-13 10:14:08 +0800

    创建朝阳分支

--- index.js
+++ index.js
@@ -26,12 +26,12 @@ var session = require('express-session');
 //}
 
 var pool=mysql.createPool({
-    host:'103.73.206.31',//qingyuankx 183.131.200.109 // 朝阳 47.95.239.253 //juyou 116.62.56.47 //ningdu 120.77.43.40//qingyuan120.76.100.224 //suzhou 121.196.221.247// songyuan 39.106.132.18 // qingyuan1213 103.73.206.31
+    host:'47.95.239.253',//qingyuankx 183.131.200.109 // 朝阳 47.95.239.253 //juyou 116.62.56.47 //ningdu 120.77.43.40//qingyuan120.76.100.224 //suzhou 121.196.221.247// songyuan 39.106.132.18 // qingyuan1213 103.73.206.31
     //user:'root',//mahjong
     //password:'123456',//a257joker
     user:'mahjong',
     password:'a257joker',//a257joker!@#Q
-    database:'mahjong_hbe',//mahjong_cy mahjong_hbe
+    database:'mahjong_cy',//mahjong_cy mahjong_hbe
     connectionLimit:10
 });
 var app=express();

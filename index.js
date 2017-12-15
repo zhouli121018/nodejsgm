73ab6c3b9a7774ff06453c2f0a1a34e0d5e9cbd0
zhouli121018 <17607619682@163.com>
2017-12-15 14:07:10 +0800

    设置session超时时间为30分钟

--- index.js
+++ index.js
@@ -39,11 +39,28 @@ var server=http.createServer(app);
 server.listen(8081);
 app.use(express.static('./public'));
 app.use(cookieParser('sessiontest'));
+
 app.use(session({
     secret: 'sessiontest',//与cookieParser中的一致
     resave: true,
-    saveUninitialized:true
+    saveUninitialized:true,
+    cookie:{
+        maxAge: 1000*60*30 // default session expiration is set to 1 hour
+    }
 }));
+
+
+app.use(function(req, res, next){
+    var url = req.path;
+    console.log(url);
+    if(url=='/login'||url=='/logout'||req.session.user){
+        req.session._garbage = Date();
+        req.session.touch();
+        next();
+    }else{
+        return res.json({"timeout":1});
+    }
+});
 
 
 var config = {

829bb1ca334db8e52599e10f8dbcbb0b4ea123b8
zhouli121018 <17607619682@163.com>
2017-12-22 18:28:10 +0800

    清远分支

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ var captchapng = require('captchapng');
 //require('body-parser-xml')(bodyParser);
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
-//process.on('uncaughtException', function (err) {
-//    //打印出错误
-//    console.log(err);
-//    //打印出错误的调用栈方便调试
-//    console.log(err.stack);
-//});
+process.on('uncaughtException', function (err) {
+   //打印出错误
+   console.log(err);
+   //打印出错误的调用栈方便调试
+   console.log(err.stack);
+});
 //try{
 //    var ok = req.params;
 //}catch(e){
